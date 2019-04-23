3dcf7f49e1d8a86f0a4ef58483e17a48d03067b7
HorusBlack <horus_black@live.com>
2019-04-23 11:34:07 -0500

    cambios: Devolucion iva 1.1

---
+++ Devolucion_Iva/src/Controllers/XmlDatos.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class XmlDatos {
+
+    private String fechaFactura, subTotal, Total, conceptoXml, folioFiscal;
+
+    public String getFechaFactura() {
+        return fechaFactura;
+    }
+
+    public void setFechaFactura(String fechaFactura) {
+        this.fechaFactura = fechaFactura;
+    }
+
+    public String getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(String subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public String getTotal() {
+        return Total;
+    }
+
+    public void setTotal(String Total) {
+        this.Total = Total;
+    }
+
+    public String getConceptoXml() {
+        return conceptoXml;
+    }
+
+    public void setConceptoXml(String conceptoXml) {
+        this.conceptoXml = conceptoXml;
+    }
+
+    public String getFolioFiscal() {
+        return folioFiscal;
+    }
+
+    public void setFolioFiscal(String folioFiscal) {
+        this.folioFiscal = folioFiscal;
+    }
+}
